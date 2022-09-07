3e916ca7e781595681ea6577f07fab8b7ed382e8
Jordin Bryant <jordintaylor095@gmail.com>
2022-09-07 16:00:08 -0400

    committing

--- server.js
+++ server.js
@@ -1,50 +1,33 @@
-require("express");
-
-console.log(require("express"))
-
 const express = require("express");
 
 const app = express();
-
-//define routes
-
+//ROUTE
 app.get("/", (req, res) => {
-  res.send("Jordin Bryant")
-})
+  res.send("Hello There");
+});
 
-app.get("/greeting", (req, res) => {
-    res.send("Hello Stranger")
-  })
+//GREETING
+app.get("/greeting/:name", (req, res) => {
+  res.send("What's up, " + req.params.name + " It's so great to see you!");
+});
 
-//Greeting
-//http://localhost:3000/greeting/Jordin
-app.get("/greeting/:name", function(req, res){
-    res.send("What's Up " + req.params.name);
-  })
+//TIP/TOTAL/TIPPERCENTAGE
 
-// //Tip Calculator
-// //http://localhost:3000/tip/100/20
-app.get("/tip/:total/:tipPercentage", function (req, res) {
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  res.send("Your tip is: " + (req.params.tipPercentage / 100) * req.params.total + "$");
+});
 
-    res.send("Your Recommended Tip " + req.params["total", "tipPercentage"]
-    )
-})
+//MAGIC-8-BALL
 
-//Magic 8 Ball
-//http://localhost:3000/magic/Will%20I%20Be%20A%20Millionaire/indexOfSayingsArray
-const sayings = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
-app.get("/magic/Will%20I%20Be%20A%20Millionaire/:indexOfSayingsArray", function (req, res) {
-
-    res.send("Magic 8 Ball Says " + (sayings[req.params.indexOfSayingsArray]))
-})
-// h1(sayings[3]); tried multiple ways to add h1 tag around the req <h1></h1> & h1 =
-
-
-
-
-
-app.listen(3000, function () {
-    console.log("express is listening for requests from the browser")
-})
+app.get("/magic/:question", (req, res) => {
+  res.send(`
+  <h1>${randomResponse}</h1>`)
+});
+const eightBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", 
+    "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
+     "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", 
+    "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", 
+    "Outlook not so good", "Very doubtful"]; 
+const randomResponse = eightBall[Math.floor(Math.random() * eightBall.length)]; 
 
 
